f5f261fbed2e745d3ae7a17d2534706b03f71723
Selidex <justindparnell@yahoo.com>
2021-04-02 22:33:25 +0000

    it was really this simple?

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# This puppet program makes a file
+file { '/tmp/holberton':
+     ensure => file,
+     owner => www-data,
+     group => www-data,
+     mode => '0744',
+     content => 'I love Puppet',
+}
