24f9a7eefb3e02fe9f3ea06ba2624563e915aa00
Selidex <justindparnell@yahoo.com>
2021-04-02 22:36:43 +0000

    lint fixes

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # This puppet program makes a file
 file { '/tmp/holberton':
-     ensure => file,
-     owner => www-data,
-     group => www-data,
-     mode => '0744',
-     content => 'I love Puppet',
+  ensure  => file,
+  owner   => www-data,
+  group   => www-data,
+  mode    => '0744',
+  content => 'I love Puppet',
 }
