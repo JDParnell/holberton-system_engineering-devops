b5efb9ebeaf7ebb8d6ed3bd2dd5d01d0afb322c6
Selidex <justindparnell@yahoo.com>
2021-04-05 04:27:15 +0000

    I have no clue at this poitn

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Puppet script to edit config file.
 
-file_line { 'Identity file':
+file_line { 'indentity_file':
   ensure => present,
   line   => 'IdentityFile ~/.ssh/holberton',
   path   => '/.ssh/config',
 }
 
-file_line { 'No password':
+file_line { 'pwd_aut':
   ensure => present,
   line   => 'PasswordAuthentication no',
   path   => '/.ssh/config',
