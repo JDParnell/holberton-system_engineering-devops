76b1b28ec779550746c90ba17cf2c9ce84363166
Selidex <justindparnell@yahoo.com>
2021-04-05 04:40:31 +0000

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Re-attempting puppet file for config use
+
+file_line { 'PasswordAuthentication':
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
+    line   => 'PasswordAuthentication no',
+}
+
+file_line { 'IdentityFile':
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
+    line   => 'IdentityFile ~/.ssh/holberton',
+}
