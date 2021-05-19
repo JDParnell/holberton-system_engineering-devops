3ca29846646ca32e0ad972a99a1f7ec24ee8e272
Selidex <justindparnell@yahoo.com>
2021-05-19 07:39:29 +0000

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" This file will be API"""
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """ gets the number of subscribers for the given subreddit"""
+    if subreddit is None:
+        return 0
+    url = "https://www.reddit.com/r/" + str(subreddit) + "/about.json"
+    headers = {'User-Agent': 'Sel'}
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 301:
+        return 0
+    rj = r.json()
+    rjd = rj.get('data')
+    return rjd.get('subscribers')
