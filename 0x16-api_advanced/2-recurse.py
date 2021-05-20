485ae9508c1f16471e207c397fb45aa31f1ddb9e
Selidex <justindparnell@yahoo.com>
2021-05-20 03:15:57 +0000

    fixed none

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,20 +7,20 @@ import sys
 def recurse(subreddit, hot_list=[]):
     """Recursion"""
     if subreddit is None:
-        print("None")
-        return
+        return None
     url = "https://www.reddit.com/r/" + str(subreddit) + "/top/.json"
     headers = {'User-Agent': 'Sel'}
     if len(hot_list) < 1:
         r = requests.get(url, headers=headers, allow_redirects=False)
+        if r.status_code != 200:
+                    return None
         after = r.json().get('data').get('after')
         hot_list.append(after)
     else:
         url = str(url) + "?after=" + str(hot_list[0])
         r = requests.get(url, headers=headers, allow_redirects=False)
     if r.status_code != 200:
-        print("None")
-        return
+        return None
     after = r.json().get('data').get('after')
     hot_list[0] = after
     rj = r.json().get('data').get('children')
